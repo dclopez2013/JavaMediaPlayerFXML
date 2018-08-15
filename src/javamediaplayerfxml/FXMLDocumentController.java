ddd0276d88cad0c80aed8d95cb0b71f4ec65f831
deepcode2013 <dclopez2013@yahoo.com>
2018-08-14 19:15:52 -0500

    Initial commit

---
+++ src/javamediaplayerfxml/FXMLDocumentController.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javamediaplayerfxml;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.*;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import javamediaplayerfxml.JavaMediaPlayerFXML;
+import sun.management.FileSystem;
+/**
+ *
+ * @author dclop
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Button play;
+       
+    @FXML
+    private Button pause;
+    
+    @FXML
+    private Button fileButton;
+    
+    @FXML
+    private Label songName;
+    
+    @FXML
+    private Label playStatus;
+    
+    public String mediaString;
+    
+    @FXML 
+    private AnchorPane ap;
+    
+    public MediaPlayer mp;
+  
+    public Media filemedia;
+    
+    @FXML
+    private MediaView mediaView;
+    
+    public static File mediaF;
+    
+    Path mPath;
+    
+    public void openFile(){
+      Alert alert = new Alert(AlertType.CONFIRMATION, "Opeing file explorer to select a file");
+      mediaF = JavaMediaPlayerFXML.fc.showOpenDialog(JavaMediaPlayerFXML.getStage());
+      Path Tstring = mediaF.toPath();
+      
+      alert.setContentText("You selected: "+ mediaF);
+      alert.showAndWait();
+      
+            // public static Media fileMedia = new Media(‪);         
+            
+      URI f = mediaF.toURI();
+      
+        try {
+            filemedia = new Media(f.toURL().toString());
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }  
+        
+         mp = new MediaPlayer(filemedia);         
+          mediaView.setMediaPlayer(mp);
+    }
+    
+     public void pauseMedia(){
+         this.setPlayStatus("Paused");
+         mp.pause();
+     }
+     
+     public void playMedia(){
+      this.setTitle();
+      this.setPlayStatus("Playing");
+     mp.play();
+     }
+    
+    
+     public void setTitle(){
+         this.songName.setText(mediaString);
+     }
+     
+     public void setPlayStatus(String s){
+         this.playStatus.setText(s);
+     }
+     
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/javamediaplayerfxml/JavaMediaPlayerFXML.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javamediaplayerfxml;
+
+import java.awt.Desktop;
+import java.io.File;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author dclop
+ */
+public class JavaMediaPlayerFXML extends Application {
+    
+    private static Stage stage;
+    
+    
+    private static void setStage(Stage stage){
+        JavaMediaPlayerFXML.stage = stage;
+    }
+    
+    public static Stage getStage(){
+        return JavaMediaPlayerFXML.stage;
+    }
+    
+    @Override
+    public void start(Stage Pstage) throws Exception {
+        
+        setStage(Pstage);
+         
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        Pstage.setScene(scene);
+        Pstage.show();
+    }
+    
+    public Desktop desktop = Desktop.getDesktop();
+    
+    public static FileChooser fc = new FileChooser();
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    
+    
+}
